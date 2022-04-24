d109e40b6e372431fd9489009c7159138806abc9
soulslurpee <charles.d.thompson87@gmail.com>
2022-04-24 16:09:55 -0400

    commit prior to deploying to Heroku

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
+const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-const apiRoutes = require('./Develop/routes/apiRoutes');
+//const apiRoutes = require('./Develop/routes/apiRoutes');
 const htmlRoutes = require('./Develop/routes/htmlRoutes')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, './Develop/public')));
 
-app.use('/api', apiRoutes);
+//app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
