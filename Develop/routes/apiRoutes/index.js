439cfb5b96f40239e489b839d54df08e4ca63e80
soulslurpee <charles.d.thompson87@gmail.com>
2022-04-24 20:42:38 -0400

    fixed error in api post middleware

--- Develop/routes/apiRoutes/index.js
+++ Develop/routes/apiRoutes/index.js
@@ -8,15 +8,15 @@ router.get('/notes', (req, res) => {
     res.json(notes);
 });
 
-router.post(function (req, res) {
-  let jsonFilePath = path.join(__dirname, "../../db/db.json");
+router.post( (req, res) => {
+  let dbPath = path.join(__dirname, "../../db/db.json");
   let newNote = req.body;
 
   newNote.id = uuidv4();
 
-  database.push(newNote)
+  notes.push(newNote)
 
-  fs.writeFile(jsonFilePath, JSON.stringify(database), (err) => {
+  fs.writeFile(dbPath, JSON.stringify(notes), (err) => {
       if (err) {
         return console.log(err);
       }
