e950d4644686e9d39f8115378ce0514be0cc6124
soulslurpee <charles.d.thompson87@gmail.com>
2022-04-24 20:25:51 -0400

    set up html and api routes

---
+++ Develop/routes/apiRoutes/index.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const notes = require('../../db/db.json');
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+router.get('/notes', (req, res) => {
+    res.json(notes);
+});
+
+router.post(function (req, res) {
+  let jsonFilePath = path.join(__dirname, "../../db/db.json");
+  let newNote = req.body;
+
+  newNote.id = uuidv4();
+
+  database.push(newNote)
+
+  fs.writeFile(jsonFilePath, JSON.stringify(database), (err) => {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("Note Saved");
+  }); 
+  res.json(newNote);
+});
--- Develop/routes/htmlRoutes/index.js
+++ Develop/routes/htmlRoutes/index.js
@@ -1,8 +1,8 @@
 const path = require('path')
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/index.html'))
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 router.get('/notes', (req, res) => {
